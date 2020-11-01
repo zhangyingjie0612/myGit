7cfebbc125146dd86065eb85aeb913b9c00f61f0
zyj <1390517441@qq.com>
2020-11-01 15:18:53 +0800

    hello2

--- src/main/java/com/jxd/follow_sys/mapper/IStudentMapper.java
+++ src/main/java/com/jxd/follow_sys/mapper/IStudentMapper.java
@@ -16,9 +16,9 @@ import java.util.Map;
  * @Version 1.0
  */
 public interface IStudentMapper extends BaseMapper<Student> {
-//    //管理员看到的学生列表
-//    List<Map<String,Object>> getStudents(@Param("courseId")List<Course> courseId, @Param("counts")Integer counts,
-//                                         @Param("pageSize")Integer pageSize, @Param("sName")String sName, @Param("cName")String cName);
     //管理员看到的学生列表
-    List<Map<String,Object>> getStudents(List<Course> courseId);
+    List<Map<String,Object>> getStudents(@Param("list")List<Course> courseId, @Param("counts")Integer counts,
+                                         @Param("pageSize")Integer pageSize);
+//    //管理员看到的学生列表
+//    List<Map<String,Object>> getStudents(List<Course> courseId);
 }
