3fbfdf3bf0d0fca942df2e3d2802591460ac8da9
zyj <1390517441@qq.com>
2020-11-02 19:01:15 +0800

    hello3

--- src/main/java/com/jxd/follow_sys/controller/StudentController.java
+++ src/main/java/com/jxd/follow_sys/controller/StudentController.java
@@ -6,11 +6,20 @@ import com.jxd.follow_sys.service.IStudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.UUID.randomUUID;
 
 /**
  * @ClassName StudentController
@@ -63,5 +72,46 @@ public class StudentController {
     @ResponseBody
     public List<Map<String,Object>> getStudentsByLike(@PathVariable("sName")String sName,@PathVariable("dept")String dept,@PathVariable("jobStr")String jobStr){
         return iStudentService.studentsListByLike(sName,dept,jobStr);
+    }
+    /**
+     * @Author: zhangyingjie
+     * @Description:学员基本信息查看
+     * @Date:9:06 2020/11/2
+     */
+    @RequestMapping("/lookStudentInfo")
+    @ResponseBody
+    public List<Map<String,Object>> lookStudentInfo(){
+        List<Course> courses = courseService.list();
+        return iStudentService.getStudents2(courses);
+    }
+    /**
+     * @Author: zhangyingjie
+     * @Description:上传学生头像
+     * @Date:17:09 2020/11/2
+     */
+    @RequestMapping("/upload")
+    @ResponseBody
+    public String doUP(@RequestParam("upload") MultipartFile upload)throws Exception{
+        //上传的位置
+        String path = "E:\\IdeaProjects\\follow_sys\\src\\main\\webapp\\static\\images\\";
+        System.out.println("path:"+path);
+        //判断，该路径是否存在
+        File file =new File(path);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        //上传文件项
+        //获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        System.out.println("filename:"+filename);
+        //把文件名称设置唯一值
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        filename=uuid+"-"+filename;
+        System.out.println("filename:"+filename);
+        //完成上传文件
+        File newFile = new File(path,filename);
+        upload.transferTo(newFile);
+        //返回文件名
+        return filename;
     }
 }
