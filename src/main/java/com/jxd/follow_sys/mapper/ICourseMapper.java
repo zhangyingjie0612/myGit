30d35f9b6cb7c4c8bfc86cba7e71ec1b57a396e9
zyj <1390517441@qq.com>
2020-11-16 15:18:02 +0800

    hell

--- src/main/java/com/jxd/follow_sys/mapper/ICourseMapper.java
+++ src/main/java/com/jxd/follow_sys/mapper/ICourseMapper.java
@@ -15,23 +15,76 @@ import java.util.Map;
  * @Version 1.0
  */
 public interface ICourseMapper extends BaseMapper<Course> {
+    /**
+     * @description: 根据课程id、课程名获取总数
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
+    int getTotalNumByLike(@Param("coursename") String coursename, @Param("courseId") String courseId);
 
-    int getTotalNumByLike(@Param("coursename") String coursename, @Param("courseid") String courseid);
+    /**
+     * @description: 根据课程id、课程名获取分页数据
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
+    List<Map<String,Object>> getPageListByLike(@Param("counts") int counts, @Param("pagesize") int pagesize, @Param("coursename") String coursename, @Param("courseId") String courseId);
 
-    List<Map<String,Object>> getPageListByLike(@Param("counts") int counts, @Param("pagesize") int pagesize, @Param("coursename") String coursename, @Param("courseid") String courseid);
-
+    /**
+     * @description: 根据课程实体类更新课程
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
     int update(Course course);
 
-    int deleteById(Integer courseid);
+    /**
+     * @description: 根据课程id删除课程
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
+    int deleteById(Integer courseId);
 
-    int deleteByIdFake(Integer courseid);
+    /**
+     * @description: 假删，根据课程id删除课程
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
+    int deleteByIdFake(Integer courseId);
 
+    /**
+     * @description: 根据课程ids批量删除课程
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
     int batchDeleteByIds(@Param("list") List<Integer> list);
 
+    /**
+     * @description: 假删，根据课程ids批量删除课程
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
     int add(Course course);
 
+    /**
+     * @description: 根据ids查找该课程是否有班级使用
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
     int ifClasscourseExist(@Param("list") List<Integer> list);
 
+    /**
+     * @description: 新增课程时验证该课程名是否存在
+     * @Author humengting
+     * @date: 2020/11/14 8:27
+     * @return
+     */
     int checkAddName(@Param("coursename") String coursename);
 
 }
